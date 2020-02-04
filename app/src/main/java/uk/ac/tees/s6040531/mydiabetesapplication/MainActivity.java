c29fe4df3fff61ffa01f0513ace198d234f7d2a5
Alicia Jade Katherine FISHER <s6040531@linux39.scm.tees.ac.uk>
2020-02-04 12:23:21 +0000

    initial commit

---
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.s6040531.mydiabetesapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
