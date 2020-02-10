32babbc9fe8c2f5164f7a319098babdd5d876385
Allicia-Jade <s6040531@live.tees.ac.uk>
2020-02-10 12:34:57 +0000

    login activity updated

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/LoginActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/LoginActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import uk.ac.tees.s6040531.mydiabetesapplication.R;
+
+public class LoginActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/SplashActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/SplashActivity.java
@@ -2,13 +2,40 @@ package uk.ac.tees.s6040531.mydiabetesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+import uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login.LoginActivity;
+
+/**
+ * SplashActivity
+ */
+public class SplashActivity extends AppCompatActivity
+{
+    // Delay for screen
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+        // Create new handler to deal with the delay and transitions
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Create new intent to go to the LoginActivity
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // Change the animation to a custom animation
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
