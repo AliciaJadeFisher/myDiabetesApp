fbe5c875f2ad47d0b64a916ee95e614924e15944
Allicia-Jade <s6040531@live.tees.ac.uk>
2020-02-10 14:12:32 +0000

    registration and setup activities created

---
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/RegistrationActivity.java
@@ -0,0 +1,18 @@
+package uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uk.ac.tees.s6040531.mydiabetesapplication.R;
+
+public class RegistrationActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
