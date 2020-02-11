2c41c0e7f6212c7dd7c37b2b6e99c3c77ee98922
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-02-11 11:35:39 +0000

    Registation Activity : layout and intialise widgets

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/RegistrationActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/RegistrationActivity.java
@@ -3,16 +3,40 @@ package uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
+/**
+ * RegistrationActivity
+ */
 public class RegistrationActivity extends AppCompatActivity
 {
+    // Variables for layout access
+    EditText etEmail, etPassword;
+    Button btnReg, btnTerms, btnLogin;
 
+    // Variable for Firebase access
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Grabs the layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Grabs the current firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        // Initialize widgets
+        etEmail = (EditText)findViewById(R.id.et_email);
+        etPassword = (EditText)findViewById(R.id.et_password);
+        btnReg = (Button) findViewById(R.id.btn_reg);
+        btnLogin = (Button)findViewById(R.id.btn_login);
+        btnTerms = (Button)findViewById(R.id.btn_login);
+
     }
 }
