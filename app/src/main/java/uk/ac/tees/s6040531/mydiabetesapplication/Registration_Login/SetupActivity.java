ba66f2d662db7f89a5cd34c4f7165e149c53db7a
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-02-11 12:29:34 +0000

    Setup Activity : carbohydrate type spinner

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/SetupActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/SetupActivity.java
@@ -2,15 +2,20 @@ package uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
-public class SetupActivity extends AppCompatActivity
+public class SetupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
-    Spinner spnBs;
+    Spinner spnBs, spnCarb;
+
+    String bs, carb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,11 +24,39 @@ public class SetupActivity extends AppCompatActivity
         setContentView(R.layout.activity_setup);
 
         spnBs = (Spinner)findViewById(R.id.spn_bs);
+        spnCarb = (Spinner)findViewById(R.id.spn_carb);
 
         ArrayAdapter<CharSequence> bsAdapter = ArrayAdapter.createFromResource(this, R.array.bs_types, android.R.layout.simple_spinner_item);
         bsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnBs.setAdapter(bsAdapter);
 
+        ArrayAdapter<CharSequence> carbAdapter = ArrayAdapter.createFromResource(this, R.array.carb_types, android.R.layout.simple_spinner_item);
+        carbAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnCarb.setAdapter(carbAdapter);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+    {
+        switch (parent.getId())
+        {
+            case R.id.spn_bs:
+                bs = parent.getItemAtPosition(position).toString();
+                System.out.println("Blood sugar type : " + bs);
+                break;
+            case R.id.spn_carb:
+                carb = parent.getItemAtPosition(position).toString();
+                System.out.println("Carbohydrate type : " + carb);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent)
+    {
 
     }
 }
