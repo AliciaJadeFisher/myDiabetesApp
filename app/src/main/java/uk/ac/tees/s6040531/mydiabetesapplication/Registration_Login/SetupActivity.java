57e7674f26a04d73e794c62a64d7b952e1defcc0
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-02-11 12:50:40 +0000

    Setup Activity : get current user

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/SetupActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/SetupActivity.java
@@ -7,22 +7,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
+/**
+ * SetupActivity
+ */
 public class SetupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
 {
+    // Variables for layout access
+    EditText etName, etHypo, etBottom, etTop, etHyper, etDuration, etPrecision, etPortion, etRatio, etCorrection;
     Spinner spnBs, spnCarb;
+    Button btnSave;
 
-    String bs, carb;
+    // Variables for user data
+    String bs, carb, id;
+
+    // Variable for database reference
+    DatabaseReference userDbRef;
+
+    // Variables for Firebase access
+    FirebaseAuth auth;
+    FirebaseUser cUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Grab the layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+        // Grabs the id of the current user
+        auth = FirebaseAuth.getInstance();
+        cUser = auth.getCurrentUser();
+        id = auth.getUid();
+        
         spnBs = (Spinner)findViewById(R.id.spn_bs);
         spnCarb = (Spinner)findViewById(R.id.spn_carb);
 
@@ -50,7 +76,6 @@ public class SetupActivity extends AppCompatActivity implements AdapterView.OnIt
                 carb = parent.getItemAtPosition(position).toString();
                 System.out.println("Carbohydrate type : " + carb);
                 break;
-
         }
     }
 
