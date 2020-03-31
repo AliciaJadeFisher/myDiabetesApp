c8bc3fcbc5047d8e82511b968baed4a7d743d1ec
Alicia-Jade <s6040531@live.tees.ac.uk>
2020-03-31 16:10:56 +0100

    Comments Done : AccountSetupActivity

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/AccountSetupActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/Registration_Login/AccountSetupActivity.java
@@ -13,39 +13,57 @@ import uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login.ui.main.Medi
 import uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login.ui.main.SectionsPagerAdapter;
 import uk.ac.tees.s6040531.mydiabetesapplication.Registration_Login.ui.main.TimeBlockFragment;
 
+/**
+ * AccountSetupActivity
+ */
 public class AccountSetupActivity extends AppCompatActivity implements BasicFragment.sendDataMedical, MedicalFragment.sendDataTime
 {
-
+    /**
+     * onCreate() method
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Retreives the relevant layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_setup);
 
+        // Initalizes the pagerAdapter
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         viewPager.setEnabled(false);
 
+        // Initializes the tabLayout
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setEnabled(false);
         tabs.setupWithViewPager(viewPager);
     }
 
+    /**
+     * sendDataMedical() method
+     * @param u
+     */
     @Override
     public void sendDataMedical(User u)
     {
+        // Sends the data to the medical fragment
         String tag = "android:switcher:" + R.id.view_pager + ":" + 1;
         MedicalFragment mf = (MedicalFragment)getSupportFragmentManager().findFragmentByTag(tag);
         mf.dataReceived(u);
     }
 
+    /**
+     * sendDataTime() method
+     * @param u
+     */
     @Override
     public void sendDataTime(User u)
     {
+        // Sends the data to the time block fragment
         String tag = "android:switcher:" + R.id.view_pager + ":" + 2;
         TimeBlockFragment tf = (TimeBlockFragment) getSupportFragmentManager().findFragmentByTag(tag);
         tf.dataReceived(u);
     }
-
 }
