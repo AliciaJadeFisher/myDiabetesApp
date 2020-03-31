08800fbce68df790904ce7e6bd5476897e43f934
Alicia-Jade <s6040531@live.tees.ac.uk>
2020-03-31 15:11:04 +0100

    Comments Done : HomeFragment

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainActivities/HomeFragment.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainActivities/HomeFragment.java
@@ -18,14 +18,25 @@ import uk.ac.tees.s6040531.mydiabetesapplication.MainActivities.EntrySection.Add
 import uk.ac.tees.s6040531.mydiabetesapplication.ObjectClasses.User;
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
+/**
+ * HomeFragment
+ */
 public class HomeFragment extends Fragment
 {
+    // Variables for layout access
     TextView tvTitle;
-    User user;
     FloatingActionButton fabAdd;
+
+    // Variable for the current user details
+    User user;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
+    /**
+     * HomeFragment constructor
+     * @param index
+     * @return fragment
+     */
     public static HomeFragment newInstance(int index)
     {
         HomeFragment fragment = new HomeFragment();
@@ -35,6 +46,10 @@ public class HomeFragment extends Fragment
         return fragment;
     }
 
+    /**
+     * onCreate() method
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -42,31 +57,56 @@ public class HomeFragment extends Fragment
 
         int index = 1;
 
+        // Checks for any arguments
         if (getArguments() != null)
         {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
     }
+
+    /**
+     * onCreateView() method
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return root
+     */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        // Grabs the relevant layout file
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
     }
 
+    /**
+     * onViewCreated() method
+     * @param view
+     * @param savedInstanceState
+     */
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
+
+        // Initializes the widgets
         tvTitle = (TextView)view.findViewById(R.id.text_home);
         fabAdd = (FloatingActionButton)view.findViewById(R.id.fab_add_entry);
 
+        // Sets the deafult welcome text
         tvTitle.setText("Welcome!");
 
+        // OnClickListener() for fabAdd
         fabAdd.setOnClickListener(new View.OnClickListener()
         {
+            /**
+             *
+             * onClick() method for fabAdd
+             * @param v
+             */
             @Override
             public void onClick(View v)
             {
+                // Loads the AddEntryActivity
                 Intent i = new Intent(getActivity(), AddEntryActivity.class);
                 startActivity(i);
                 getActivity().overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
@@ -75,17 +115,16 @@ public class HomeFragment extends Fragment
         });
     }
 
+    /**
+     * setUser() method
+     * @param u
+     */
     public void setUser(User u)
     {
-        if(u != null)
-        {
+        // Grabs the passed in user and sets the welcome text
+        if(u != null) {
             user = u;
-
             tvTitle.setText("Welcome " + user.getName());
-
-            System.out.println("======================== Current user : " + user.getName() + "====================");
         }
     }
-
-
 }
