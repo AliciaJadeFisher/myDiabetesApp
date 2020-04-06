fcb99742610ecb9e02ac7bb39d8ec6038f16f3ab
Alicia-Jade <s6040531@live.tees.ac.uk>
2020-04-06 17:16:46 +0100

    SearchCarbActivity : layout done

---
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainActivities/EntrySection/SearchCarbActivity.java
@@ -0,0 +1,25 @@
+package uk.ac.tees.s6040531.mydiabetesapplication.MainActivities.EntrySection;
+
+import androidx.appcompat.app.AppCompatActivity;
+import uk.ac.tees.s6040531.mydiabetesapplication.R;
+
+import android.os.Bundle;
+
+public class SearchCarbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_carb);
+    }
+
+    /**
+     * onBackPressed() method
+     */
+    @Override
+    public void onBackPressed()
+    {
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+        finish();
+    }
+}
