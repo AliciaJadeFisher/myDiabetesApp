5976c4ee775c8cb2f180edd542cf616eb0da9443
Alicia-Jade <s6040531@live.tees.ac.uk>
2020-04-14 15:04:20 +0100

    SearchCarbActivity : api request coded done, activity ready for testing

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/EntrySection/SearchCarbActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/EntrySection/SearchCarbActivity.java
@@ -12,6 +12,16 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * SearchCarbActivity
  */
@@ -77,6 +87,46 @@ public class SearchCarbActivity extends AppCompatActivity
      */
     public void getApiResult(String s)
     {
+        // Create a new request queue
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Replace all spaces with '+'
+        String search = s.replaceAll("\\s","+");
+
+        // Create the request url to return JSON data
+        final String url = "https://world.openfoodsfacts.org/cgi.search.p1?search_terms=" + search +"&search_simple=1&action=process&json=1";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println("========== Search url : " + url);
+                        System.out.println("========== API Respon : " + response);
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+
+                            adapter = new FoodListViewAdapter(SearchCarbActivity.this, object.getJSONArray("products"));
+                            lvResults.setAdapter(adapter);
+
+                            tvDisplay.setText("No. results : " + adapter.getCount());
+                        } catch (JSONException e) {
+                            Toast.makeText(SearchCarbActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        queue.stop();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(SearchCarbActivity.this, "API response : " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        queue.stop();
+                    }
+                });
+
+        queue.add(stringRequest);
     }
 
     /**
