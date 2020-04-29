1036da263874867e856dbda143f2b4ab1712eb3d
Alicia-Jade <s6040531@live.tees.ac.uk>
2020-04-29 14:26:22 +0100

    ViewRecordActivity: layout done

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/HomeSection/ViewRecordActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/HomeSection/ViewRecordActivity.java
@@ -1,15 +1,19 @@
 package uk.ac.tees.s6040531.mydiabetesapplication.MainSections.HomeSection;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewRecordActivity extends AppCompatActivity
 {
+    TextView tvDate, tvTime, tvBs, tvCarbs, tvMeal, tvInF, tvInC, tvInT, tvNote;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_record);
     }
