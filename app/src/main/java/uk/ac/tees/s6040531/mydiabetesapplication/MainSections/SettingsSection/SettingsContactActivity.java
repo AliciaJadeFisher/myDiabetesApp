f8f11f288094177b4be063ab19b3aaba136b3d11
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-01 18:15:22 +0100

    SettingsContactActivity : created

---
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsContactActivity.java
@@ -0,0 +1,18 @@
+package uk.ac.tees.s6040531.mydiabetesapplication.MainSections.SettingsSection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uk.ac.tees.s6040531.mydiabetesapplication.R;
+
+public class SettingsContactActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_contact);
+    }
+}
