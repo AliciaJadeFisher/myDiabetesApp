36ca971e7c1853af8d2990c7b5c45781b60a45da
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-01 18:20:13 +0100

    SettingsHelpActivity : button listeners added

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsHelpActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsHelpActivity.java
@@ -2,7 +2,9 @@ package uk.ac.tees.s6040531.mydiabetesapplication.MainSections.SettingsSection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
@@ -19,5 +21,30 @@ public class SettingsHelpActivity extends AppCompatActivity
 
         btnFAQ = (Button)findViewById(R.id.btn_faq);
         btnContact = (Button)findViewById(R.id.btn_contact);
+
+        btnFAQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SettingsHelpActivity.this, SettingsFAQActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+            }
+        });
+
+        btnContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SettingsHelpActivity.this, SettingsContactActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+        finish();
     }
 }
