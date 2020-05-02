af24f8d504c5d308b35346a498af8bed89426faf
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-02 13:44:36 +0100

    SettingsFragment : deleteAccount alert dialog done

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsFragment.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsFragment.java
@@ -1,28 +1,50 @@
 package uk.ac.tees.s6040531.mydiabetesapplication.MainSections.SettingsSection;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.MainSections.AuthenticationSection.AccountSetupActivity;
 import uk.ac.tees.s6040531.mydiabetesapplication.MainSections.AuthenticationSection.LoginActivity;
+import uk.ac.tees.s6040531.mydiabetesapplication.ObjectClasses.User;
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
 public class SettingsFragment extends Fragment
 {
-    Button btnEdit, btnLogOut, btnAbout, btnHelp;
+    Button btnEdit, btnDelete, btnAbout, btnHelp, btnLogOut;
+
+    AlertDialog.Builder deleteAccount, checkDelete;
 
     FirebaseAuth auth;
+    FirebaseUser cUser;
+    AuthCredential cred;
+    FirebaseFirestore uDbRef;
+
+    User user;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
@@ -57,10 +79,16 @@ public class SettingsFragment extends Fragment
     {
         super.onViewCreated(view, savedInstanceState);
 
+        SharedPreferences myPref = getActivity().getSharedPreferences(getResources().getString(R.string.pref_key), Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = myPref.getString(getResources().getString(R.string.user_key),"");
+        user = gson.fromJson(json,User.class);
+
         btnEdit = (Button)view.findViewById(R.id.btn_edit);
-        btnLogOut = (Button)view.findViewById(R.id.btn_log_out);
+        btnDelete = (Button)view.findViewById(R.id.btn_delete);
         btnAbout = (Button)view.findViewById(R.id.btn_about);
         btnHelp = (Button)view.findViewById(R.id.btn_help);
+        btnLogOut = (Button)view.findViewById(R.id.btn_log_out);
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,15 +102,10 @@ public class SettingsFragment extends Fragment
             }
         });
 
-        btnLogOut.setOnClickListener(new View.OnClickListener()
-        {
+        btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                auth.signOut();
-                getActivity().startActivity(new Intent(getActivity(), LoginActivity.class));
-                getActivity().overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                getActivity().finish();
+            public void onClick(View v) {
+                deleteAccount.show();
             }
         });
 
@@ -105,5 +128,53 @@ public class SettingsFragment extends Fragment
                 getActivity().overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
             }
         });
+
+        btnLogOut.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                auth.signOut();
+                getActivity().startActivity(new Intent(getActivity(), LoginActivity.class));
+                getActivity().overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                getActivity().finish();
+            }
+        });
+
+        deleteAccount = new AlertDialog.Builder(getActivity());
+        deleteAccount.setTitle("Delete Account");
+        final EditText dPassword = new EditText(getActivity());
+        dPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        deleteAccount.setView(dPassword);
+        deleteAccount.setPositiveButton("Delete", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                cred = EmailAuthProvider.getCredential(auth.getCurrentUser().getEmail(), dPassword.getText().toString());
+                cUser.reauthenticate(cred).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(!task.isSuccessful())
+                        {
+                            Toast.makeText(getActivity(),"Incorrect Password", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            checkDelete.show();
+                        }
+                    }
+                });
+            }
+        });
+        deleteAccount.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        checkDelete = new AlertDialog.Builder(getActivity());
+
     }
 }
