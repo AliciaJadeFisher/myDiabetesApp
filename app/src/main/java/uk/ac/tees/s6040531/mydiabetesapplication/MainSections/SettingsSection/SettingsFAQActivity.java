643506af1a607582eda8cd41d9831efd6fa9096e
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-04 18:22:33 +0100

    SettingsFAQActivity : commented and warnings dealt with

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsFAQActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsFAQActivity.java
@@ -6,16 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
-public class SettingsFAQActivity extends AppCompatActivity {
-
+/**
+ * SettingsFAQActivity
+ */
+public class SettingsFAQActivity extends AppCompatActivity
+{
+    /**
+     * onCreate() method
+     * @param savedInstanceState - instance bundle
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // Grabs the relevant layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_faq);
     }
 
+    /**
+     * onBackPressed() method
+     */
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
+        // Returns to the previous activity
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         finish();
