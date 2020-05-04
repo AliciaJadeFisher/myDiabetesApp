500e7cf32f6d7673c86e93280eb5b96efbe9b096
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-04 17:47:26 +0100

    SearchNetworkChecker : commented and warnings dealt with

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/EntrySection/SearchNetworkChecker.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/EntrySection/SearchNetworkChecker.java
@@ -6,13 +6,24 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import java.util.Objects;
+
 import static uk.ac.tees.s6040531.mydiabetesapplication.MainSections.EntrySection.SearchCarbActivity.showSearch;
 
+/**
+ * SearchNetworkChecker class
+ */
 public class SearchNetworkChecker extends BroadcastReceiver
 {
+    /**
+     * onReceive() method
+     * @param context - activity called from
+     * @param intent - intent passed
+     */
     @Override
     public void onReceive(Context context, Intent intent)
     {
+        // Checks if there is a connection and passes the result back to the activity
         try
         {
             if(hasConnection(context))
@@ -31,13 +42,20 @@ public class SearchNetworkChecker extends BroadcastReceiver
     }
 
 
+    /**
+     * hasConnection() method
+     * @param context - activity called from
+     * @return connection
+     */
     private boolean hasConnection(Context context)
     {
+        // Gets network information
         try
         {
             ConnectivityManager conMan =(ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo netInf = conMan.getActiveNetworkInfo();
+            NetworkInfo netInf = Objects.requireNonNull(conMan).getActiveNetworkInfo();
 
+            // Returns if there is a connection or not
             return(netInf != null && netInf.isConnected());
         }
         catch(NullPointerException e)
