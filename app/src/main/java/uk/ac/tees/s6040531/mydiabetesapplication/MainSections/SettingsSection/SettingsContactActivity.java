402d7290f363ccbadea7951241762364e231c781
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-04 18:21:34 +0100

    SettingsContactActivity : commented and warnings dealt with

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsContactActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsContactActivity.java
@@ -6,18 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
+/**
+ * SettingsContactActivity
+ */
 public class SettingsContactActivity extends AppCompatActivity
 {
-
+    /**
+     * onCreate() method
+     * @param savedInstanceState - instance bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Grabs the relevant layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_contact);
     }
 
+    /**
+     * onBackPressed() method
+     */
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
+        // Returns to the previous activity
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         finish();
