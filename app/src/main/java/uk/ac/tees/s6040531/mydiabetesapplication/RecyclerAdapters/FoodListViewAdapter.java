5a2953b6507454e9fe014d03fbaba11b459c9130
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-05 15:29:48 +0100

    FoodListViewAdapter : commented and warnings dealt with

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/RecyclerAdapters/FoodListViewAdapter.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/RecyclerAdapters/FoodListViewAdapter.java
@@ -1,5 +1,6 @@
 package uk.ac.tees.s6040531.mydiabetesapplication.RecyclerAdapters;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -24,15 +25,15 @@ import uk.ac.tees.s6040531.mydiabetesapplication.R;
 public class FoodListViewAdapter extends BaseAdapter
 {
     // ListViewAdapter attributes
-    Context context;
-    LayoutInflater inflater;
-    List<FoodItem> productList = new ArrayList<>();
+    private Context context;
+    private LayoutInflater inflater;
+    private List<FoodItem> productList;
 
     /**
      * Main constructor
-     * @param c
-     * @param fl
-     * @throws JSONException
+     * @param c - context called from
+     * @param fl - list of food products
+     * @throws JSONException - deals with JSON exceptions
      */
     public FoodListViewAdapter(Context c, JSONArray fl) throws JSONException
     {
@@ -52,15 +53,16 @@ public class FoodListViewAdapter extends BaseAdapter
 
     /**
      * getView() method
-     * @param position
-     * @param view
-     * @param parent
+     * @param position - list position
+     * @param view - view for listView
+     * @param parent - parent view group
      * @return view
      */
+    @SuppressLint("InflateParams")
     @Override
     public View getView(final int position, View view, ViewGroup parent)
     {
-        final Activity activity = (Activity)context;
+        // Declares holder
         final ViewHolder holder;
 
         // Checks if the view is null or not
@@ -71,10 +73,11 @@ public class FoodListViewAdapter extends BaseAdapter
             view = inflater.inflate(R.layout.listview_food_result, null);
 
             // Initializes the layout widgets
-            holder.tvName = (TextView)view.findViewById(R.id.tv_product_name);
-            holder.tvCarbs100 = (TextView)view.findViewById(R.id.tv_carbs_100);
-            holder.tvCarbsServ = (TextView)view.findViewById(R.id.tv_carbs_serving);
+            holder.tvName = view.findViewById(R.id.tv_product_name);
+            holder.tvCarbs100 = view.findViewById(R.id.tv_carbs_100);
+            holder.tvCarbsServ = view.findViewById(R.id.tv_carbs_serving);
 
+            // Attaches the holder
             view.setTag(holder);
         }
         else
@@ -87,20 +90,23 @@ public class FoodListViewAdapter extends BaseAdapter
 
         // Sets the textView texts for each list item
         holder.tvName.setText(food.getName());
-        holder.tvCarbs100.setText("Carbohydrates per 100g : " + food.getCarbs_100() + "g");
-        holder.tvCarbsServ.setText("Carbohydrates per serving (" + food.getServing() + "g) : " + food.getCarbs_serv() + "g");
+        String c100_display = "Carbohydrates per 100g : " + food.getCarbs_100() + "g";
+        String cServ_display = "Carbohydrates per serving (" + food.getServing() + "g) : " + food.getCarbs_serv() + "g";
+        holder.tvCarbs100.setText(c100_display);
+        holder.tvCarbsServ.setText(cServ_display);
 
         return view;
     }
 
     /**
      * getFoods() method
-     * @param products
+     * @param products - list of products
      * @return foods
-     * @throws JSONException
+     * @throws JSONException - deals with JSON exceptions
      */
-    public List<FoodItem> getFoods(JSONArray products) throws JSONException
+    private List<FoodItem> getFoods(JSONArray products) throws JSONException
     {
+        // New list
         List<FoodItem> foods = new ArrayList<>();
 
         // Loops through the JSONArray
@@ -109,24 +115,20 @@ public class FoodListViewAdapter extends BaseAdapter
             // Grabs the current JSONObject
             JSONObject product = products.getJSONObject(i);
 
-            String n;
-            System.out.println("=========== Product : " + product);
             // Gets the product name
+            String n;
             try
             {
                 n = product.getString("product_name");
-                System.out.println("=============== Product Name : " + n + "===============");
             }
             catch(JSONException e)
             {
                 n="n/a";
             }
 
-
+            // Gets the product details
             int s;
             double c,cs;
-
-            // Gets the product details
             try
             {
                 s = product.getInt("serving_quantity");
@@ -177,7 +179,7 @@ public class FoodListViewAdapter extends BaseAdapter
 
     /**
      * getItem() method
-     * @param position
+     * @param position - position in list
      * @return productList.get(position)
      */
     @Override
@@ -188,7 +190,7 @@ public class FoodListViewAdapter extends BaseAdapter
 
     /**
      * getItemId() method
-     * @param position
+     * @param position - position in list
      * @return position
      */
     @Override
