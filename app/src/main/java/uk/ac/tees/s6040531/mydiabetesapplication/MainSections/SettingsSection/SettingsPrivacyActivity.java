4e6652e556c8f6881af825962fbf3686f6dc6395
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-05 15:05:00 +0100

    SettingsPrivacyActivity : commented and warnings dealt with

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsPrivacyActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsPrivacyActivity.java
@@ -6,16 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
+/**
+ * SettingsPrivacyActivity
+ */
 public class SettingsPrivacyActivity extends AppCompatActivity {
 
+    /**
+     * onCreate() method
+     * @param savedInstanceState - instance bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Grabs the relevant layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_privacy);
     }
 
+    /**
+     * onBackPressed() method
+     */
     @Override
     public void onBackPressed() {
+        // Returns to the previous activity
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         finish();
