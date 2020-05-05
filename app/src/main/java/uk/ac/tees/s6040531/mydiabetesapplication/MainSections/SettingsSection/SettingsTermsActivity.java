f5b42c17937d3031472423828500b302ee25b5c8
Alicia-Jade Fisher <s6040531@live.tees.ac.uk>
2020-05-05 15:05:57 +0100

    SettingsTermsActivity : commented and warnings dealt with

--- app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsTermsActivity.java
+++ app/src/main/java/uk/ac/tees/s6040531/mydiabetesapplication/MainSections/SettingsSection/SettingsTermsActivity.java
@@ -6,18 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import uk.ac.tees.s6040531.mydiabetesapplication.R;
 
+/**
+ * SettingsTermsActivity
+ */
 public class SettingsTermsActivity extends AppCompatActivity
 {
-
+    /**
+     * onCreate() method
+     * @param savedInstanceState - instance bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        // Grabs the relevant layout file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_terms);
     }
 
+    /**
+     * onBackPressed() method
+     */
     @Override
     public void onBackPressed() {
+        // Returns to the previous activity
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         finish();
